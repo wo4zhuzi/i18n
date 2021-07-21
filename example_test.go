855a96b13a0041010416d758b8a9ee5f9864f2db
david <david@legenddigital.com.au>
2021-07-21 23:23:45 +0800

    add option language

--- example_test.go
+++ example_test.go
@@ -17,4 +17,12 @@ func TestAdd(t *testing.T) {
 
 	t.Log(str1)
 
+	str2 := i18nClient.TOption("人生最幸福的时期，是什么时候？", "ko")
+
+	t.Log(str2)
+
+	str3 := i18nClient.TOption("%s年欧洲杯冠军是%s", "ko", "2021", "이탈리아")
+
+	t.Log(str3)
+
 }
--- i18n.go
+++ i18n.go
@@ -7,6 +7,7 @@ import (
 )
 
 type I18n struct {
+	Path   string
 	Lang   string
 	Source [][]string
 	Data   map[string]string
@@ -14,7 +15,12 @@ type I18n struct {
 
 func NewI18n(path string, lang string) (*I18n, error) {
 	fileName := fmt.Sprintf("%s/%s.csv", path, lang)
-	fs1, _ := os.Open(fileName)
+	fs1, err := os.Open(fileName)
+
+	if err != nil {
+		return nil, err
+	}
+
 	r1 := csv.NewReader(fs1)
 	r1.Comma = ','
 	r1.FieldsPerRecord = -1
@@ -24,6 +30,7 @@ func NewI18n(path string, lang string) (*I18n, error) {
 	}
 
 	i18n := I18n{}
+	i18n.Path = path
 	i18n.Lang = lang
 	i18n.Source = content
 	i18n.Data = make(map[string]string)
@@ -44,6 +51,27 @@ func (i *I18n) T(key string, args ...interface{}) string {
 			}
 		}
 	}
+	format = i.preArgs(format, args...)
+	return format
+}
+
+//Choose language translation
+func (i *I18n) TOption(key string, lang string, args ...interface{}) string {
+	i18nClient, err := NewI18n(i.Path, lang)
+	format := key
+
+	if err != nil {
+		return i.preArgs(format, args...)
+	}
+
+	for _, row := range i18nClient.Source {
+		if row[0] == key {
+			i18nClient.Data[key] = row[1]
+			format = row[1]
+			break
+		}
+	}
+
 	format = i.preArgs(format, args...)
 	return format
 }
